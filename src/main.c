be128e4f696069c9aac198a2c52c6b887a3067cb
thatretrodev <thatretrodev@outlook.com>
2021-08-09 22:36:03 -0400

    Clean up main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
 #include <gb/gb.h>
 #include <stdint.h>
 
-
-void main(void)
-{
-    // Loop forever
-    while(1) {
-
-
-		// Game main loop processing goes here
-
-
-		// Done processing, yield CPU and wait for start of next frame
-        wait_vbl_done();
-    }
+void main() {
+	while (1) {
+		wait_vbl_done();
+	}
 }
