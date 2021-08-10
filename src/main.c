327c4fa56391a05823d154bf020dc8a58df8bd52
thatretrodev <thatretrodev@outlook.com>
2021-08-09 22:53:26 -0400

    Add some changes to main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <gb/gb.h>
 #include <stdint.h>
+#include <stdio.h>
+
+void clear_text() {
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
 
 void main() {
+	int frame_counter;
+
+	printf("____________________\nGBDemo\n - by thatretrodev\n____________________\n");
 	while (1) {
+		if (frame_counter > 150) {
+			clear_text();
+		}
 		wait_vbl_done();
+		frame_counter++;
 	}
 }
